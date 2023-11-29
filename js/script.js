36aa4eb1ee3e3d91090044128b06ce245fedb1c8
KurniaKrisnaRizky <kurnia0989@gmail.com>
2023-11-30 02:39:48 +0700

    percobaan tampilan mobile

--- js/script.js
+++ js/script.js
@@ -1,34 +1,54 @@
-let button_mobil = document.querySelector(".service-content-mobil ");
-let button_motor = document.querySelector(".service-content-motor ");
+let button_mobil = document.querySelector(".service-content-mobil");
+let button_motor = document.querySelector(".service-content-motor");
 let title_pemesanan = document.querySelector(".title-pemesanan");
+let menu = document.querySelector("#menu");
+let navLink = document.querySelector(".nav-link");
 
 button_mobil.addEventListener("click", () => {
   title_pemesanan.innerHTML = "Pemesanan Sewa Mobil";
   let form_pemesanan = document.querySelector("#form-pemesanan");
   form_pemesanan.action = "list-mobil.html";
+  document
+    .querySelector(".service-content-mobil button")
+    .classList.add("active");
+  document
+    .querySelector(".service-content-motor button")
+    .classList.remove("active");
 });
 
 button_motor.addEventListener("click", () => {
   title_pemesanan.innerHTML = "Pemesanan Sewa Motor";
   let form_pemesanan = document.querySelector("#form-pemesanan");
   form_pemesanan.action = "list-motor.html";
+  document
+    .querySelector(".service-content-motor button")
+    .classList.add("active");
+  document
+    .querySelector(".service-content-mobil button")
+    .classList.remove("active");
 });
 
-let tanggal = document.querySelector("#tanggal");
-let month = [
-  "Januari",
-  "Febuari",
-  "Maret",
-  "April",
-  "Mei",
-  "Juni",
-  "Juli",
-  "Agustus",
-  "September",
-  "Oktober",
-  "November",
-  "Desember",
-];
+
+
+document.querySelector("#menu").onclick = () => {
+  navLink.classList.toggle("active2");
+};
+
+// let tanggal = document.querySelector("#tanggal");
+// let month = [
+//   "Januari",
+//   "Febuari",
+//   "Maret",
+//   "April",
+//   "Mei",
+//   "Juni",
+//   "Juli",
+//   "Agustus",
+//   "September",
+//   "Oktober",
+//   "November",
+//   "Desember",
+// ];
 
 // tanggal.innerHTML = month[new Date().getMonth()];
 
