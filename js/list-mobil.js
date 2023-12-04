7c16a004bd09406818efee9ac209f7d7c785762e
KurniaKrisnaRizky <kurnia0989@gmail.com>
2023-12-04 16:51:47 +0700

    Second Commit

---
+++ js/list-mobil.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector("#menu");
+let navLink = document.querySelector(".nav-link");
+
+
+document.querySelector("#menu").onclick = () => {
+    navLink.classList.toggle("active2");
+  };
+  
