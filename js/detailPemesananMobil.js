84f816f14ef97b04ad32b5501c3a6088d1d69041
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2023-12-11 13:12:54 +0700

    ""

--- js/detailPemesananMobil.js
+++ js/detailPemesananMobil.js
@@ -63,7 +63,7 @@ pemesan.addEventListener("click", (event) => {
           'email'           : document.querySelector('#email').value,
           'nomorHandphone'  : document.querySelector('#no-hp').value,
           'tabelMobilId'    : idMobil,
-          'tabelMotorId'    : "",
+          'tabelMotorId'    : idMotor,
           'tabelpesananId'  : idPesanan
         })
     })
